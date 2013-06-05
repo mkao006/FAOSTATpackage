6df208669b00e1e12227bbfbfe0f87125bea11b9
filippogheri <filippogheri@hotmail.com>
2013-06-05 15:44:32 +0200

    added another silent = TRUE to the lsgr function

--- Codes/lsgr.R
+++ Codes/lsgr.R
@@ -46,7 +46,7 @@ lsgr = function(x, n = 1){
                 lsgr[1:(firstObs + n - 1)] = NA
                 for(i in (firstObs + n):T){
                     tmp = try((exp(coef(rlm(log(x[(i - n):(i)]) ~ t))[2]) - 1) *
-                      100)
+                      100, silent = TRUE)
                     if(!inherits(tmp, "try-error")){
                         lsgr[i] = tmp
                     } else {
