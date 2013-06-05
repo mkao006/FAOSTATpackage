a53ac0e0fdd46578bd3836ddcbe09429e66d893a
filippogheri <filippogheri@hotmail.com>
2013-06-05 09:01:51 +0200

    I changed the server for faostatprod because the one we were using was instable

--- Codes/getFAO.R
+++ Codes/getFAO.R
@@ -45,11 +45,10 @@ getFAO = function(name = NULL, domainCode = "RL", elementCode = 5110,
         name = paste(domainCode, itemCode, elementCode, sep = "_")
 
     if(productionDB){
-        ## base = "http://ldvapp07.fao.org:8030/wds/api?"
-        ## changed by FILIPPO new repository
-        ## base = "http://lprapp16.fao.org:4012/wds/api?"
-        ## changed by FILIPPO again
-        base = "http://lprapp16.fao.org/wds/api?"
+#         base = "http://ldvapp07.fao.org:8030/wds/api?"
+#         base = "http://lprapp16.fao.org:4012/wds/api?"
+#         base = "http://lprapp16.fao.org/wds/api?"
+        base = "http://ldvapp07.fao.org:8032/wds/"
 
         database = "db=faostatprod&"
         selection = "select=D.AreaCode[FAOST_CODE],D.Year[Year],D.Value[Value],&from=data[D],element[E]&"
