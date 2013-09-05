070f85bf7e70e4f0e181194276ef9182a075a859
filippogheri <filippogheri@hotmail.com>
2013-09-05 11:46:19 +0200

    Changing again the FAOSTATprod base

--- FAOSTAT/R/getFAO.R
+++ FAOSTAT/R/getFAO.R
@@ -47,10 +47,10 @@ getFAO = function(name = NULL, domainCode = "RL", elementCode = 5110,
         name = paste(domainCode, itemCode, elementCode, sep = "_")
 
     if(productionDB){
-#         base = "http://ldvapp07.fao.org:8030/wds/api?"
+        base = "http://ldvapp07.fao.org:8030/wds/api?"
 #         base = "http://lprapp16.fao.org:4012/wds/api?"
 #         base = "http://lprapp16.fao.org/wds/api?"
-        base = "http://ldvapp07.fao.org:8032/wds/api?"
+#         base = "http://ldvapp07.fao.org:8032/wds/api?"
 
         database = "db=faostatprod&"
         selection = "select=D.AreaCode[FAOST_CODE],D.Year[Year],D.Value[Value],&from=data[D],element[E]&"
