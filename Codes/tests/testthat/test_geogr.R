1958ae1338c8d754166c6bb4da0c2fd4c7f05fb0
mk <mkao006@gmail.com>
2016-02-23 10:55:37 +0800

    changed expect_that() function to expect_xx() specific function as recommended

--- Codes/tests/testthat/test_geogr.R
+++ Codes/tests/testthat/test_geogr.R
@@ -2,8 +2,8 @@ context("Testing function geogr()")
 
 
 test_that("Function geogr() generate correct value", {
-    expect_that(geogr(1), gives_warning())
-    expect_that(geogr(c(1, 1)), gives_warning())
+    expect_warning(geogr(1), "Time series does not have sufficient values")
+    expect_warning(geogr(c(1, 1)), "Time series does not have sufficient values")
     expect_equal(geogr(rep(1, 5)), c(NA, rep(0, 4)))
     expect_equal(geogr(c(1, 2, 4, 8, 16)), c(NA, rep(100, 4)))
     expect_equal(geogr(1:5), c(NA, 1/1:4) * 100)
@@ -12,7 +12,7 @@ test_that("Function geogr() generate correct value", {
 
 test_that("Function geogr() gives correct output", {
     ## Testing class
-    expect_that(geogr(1:5), is_a("numeric"))
+    expect_is(geogr(1:5), "numeric")
 
     ## Testing whether the right number of NA is filled
     expect_equal(length(na.omit(geogr(1:5))), 4)
